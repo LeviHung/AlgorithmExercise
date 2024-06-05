4e03c9467bd0602b0293bd10c45445d065953570
Ming Chung Hung <124744041+LeviHung@users.noreply.github.com>
2024-06-05 05:48:40 +0000

    Add a framework to expand the exercises of Algorithm.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,63 @@
 // 06/04/24
 // Algorthim in Udemy course
 
+import java.util.Scanner;
 // What is the Big O of the below function? 
 // (Hint, you may want to go line by line)
 // ANS: It depends on anotherFunction function. If there is not a function
 // in the funChallege, the big O will be O(n);
 
+
 public class Main {
   public static void main(String[] args) {
+    int inputKey;
+    boolean result;
+
+    // Create a Scanner object for keyboard input.
+    Scanner keyboard = new Scanner(System.in);
+
+    do {
+      // Display a console menu. 
+      displayMenu();
+
+      // Get the user's input.
+      inputKey = keyboard.nextInt();
+      keyboard.nextLine();
+
+      switch (inputKey) {
+        case 1:
+          firstExercise();
+          break;
+        case 2:
+          break;
+        default:
+          if (inputKey != 0) {
+            System.out.println("Invalid Input!");
+          }
+          break;
+      }
+    } while (inputKey != 0);
+  }
+    
+  /**
+    * The displayMenu method displays a console menu.
+    */
+  public static void displayMenu()
+  {
+    System.out.println("\n==============================");
+    System.out.println("1. First Algorthm Exercise");
+    System.out.println("2. Recursive Fibonacci Timer");
+    System.out.println("0. Exit");
+    System.out.println("==============================");
+    System.out.print("Enter a choice: ");
+  }
+
+  public static void firstExercise()
+  {    
     System.out.println("First Algorithm Exercise!");
     int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     int result = funChallenge(array);
-    System.out.print("result = " + result);
+    System.out.println("result = " + result);
   }
 
   public static int funChallenge(int [] input)
@@ -20,13 +66,13 @@ public class Main {
     int a = 10;
     a = 50 + 3;
 
-    for (int i = 0; i < input.length; i++)
-    {
+    for (int i = 0; i < input.length; i++) {
       // call another Function
       // anothorFunction();
       boolean stranger = true;
       a ++;
     }
+
     return a;
   }
 }
